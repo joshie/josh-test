315b1ccc7a5daa1c2f6a1ac15ca6161162075ad8
joshie <joshlohrman@gmail.com>
2019-03-27 13:43:49 -0400

    make url more usable

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const request = require('request')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => request('http://'+process.env.API_URL+':8081', function(e,r,b) {
+app.get('/', (req, res) => request(process.env.API_URL, function(e,r,b) {
   res.send('Hello World! API Data:' + b)
 }));
 
