0c054ed7a2041ff29a3c81a694c910b8cf4734a1
joshie <joshlohrman@gmail.com>
2019-03-27 13:31:29 -0400

    testing a thing

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const request = require('request')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => request('http://'+process.env.API_URL+':8081', function(e,r,b) {
+  res.send('Hello World! ' + b)
+}));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
