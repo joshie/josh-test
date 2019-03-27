7f9b6156f48a1a8d5e5bef62c0a2256121d00d1c
joshie <joshlohrman@gmail.com>
2019-03-27 16:18:34 -0400

    yay

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => request(process.env.API_URL, function(e,r,b) {
-  res.send('Hello World! API Data:' + b)
+  res.send('Hello USA! API Data:' + b)
 }));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
